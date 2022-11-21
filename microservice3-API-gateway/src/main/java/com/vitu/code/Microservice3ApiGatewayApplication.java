f3d9fb690debc1249bbdce81f10ee403502d36e8
vithaln <vnnivargi1998@gmail.com>
2022-11-21 23:02:07 +0530

    intial commit

---
+++ microservice3-API-gateway/src/main/java/com/vitu/code/Microservice3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vitu.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice3ApiGatewayApplication.class, args);
+	}
+
+}
